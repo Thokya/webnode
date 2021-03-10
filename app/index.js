a7202d34a570c7d0219ee8cb23c2c6a414d1c4a7
Abhishek <abhishekmore021@gmail.com>
2021-03-10 14:51:18 +0530

    adding

--- app/index.js
+++ app/index.js
@@ -16,7 +16,8 @@ const websocket = new WebSocketServer({
 httpserver.listen(4000, () => console.log(`listening on localhost:4000`));
 
 websocket.on("request", request => {
-    const con = request.accept(null, request.origin)
+    let con = request.accept(null, request.origin)
+
     con.on("open", () => console.log("opened"))
     con.on("close", () => console.log("CLOSED!!!"))
     con.on("message", message => {
@@ -26,3 +27,10 @@ websocket.on("request", request => {
     });
     //setInterval(() => con.send("connection active"), 10000)
 });
+
+// let ws = new WebSocket("ws://localhost:4000");
+// ws.onmessage = message => console.log(`Received: ${message.data}`);
+// ws.send("Hello! I'm client")
+
+
+// https://intense-scrubland-56988.herokuapp.com/
